ad99d34c33aed4525ad07cafb378d61d6a141a87
Una Ada <trewbot@gmail.com>
2025-08-15 09:40:33 -0700

    Yeet Sodium compat pt.2

--- common/src/main/java/de/mrjulsen/paw/event/ModClientEvents.java
+++ common/src/main/java/de/mrjulsen/paw/event/ModClientEvents.java
@@ -1,13 +1,10 @@
 package de.mrjulsen.paw.event;
 
 import de.mrjulsen.paw.PantographsAndWires;
-import de.mrjulsen.paw.compat.sodium.IncompatabilityScreen;
-import de.mrjulsen.paw.compat.sodium.SodiumCompatEvent;
 import de.mrjulsen.wires.item.WireBaseItem;
 import de.mrjulsen.wires.render.WireRenderer;
 import de.mrjulsen.wires.WireClientNetwork;
 import de.mrjulsen.wires.WireNetwork;
-import dev.architectury.event.CompoundEventResult;
 import dev.architectury.event.events.client.ClientGuiEvent;
 import dev.architectury.event.events.client.ClientLifecycleEvent;
 import dev.architectury.event.events.client.ClientPlayerEvent;
@@ -67,16 +64,6 @@ public final class ModClientEvents {
                 }
             }
         });
-
-        if (PantographsAndWires.isSodiumLoaded()) {
-            SodiumCompatEvent.init();
-
-            if (!PantographsAndWires.isIndiumLoaded()) {
-                ClientGuiEvent.SET_SCREEN.register((screen) -> {
-                    return CompoundEventResult.interruptTrue(new IncompatabilityScreen());
-                });
-            }
-        }
     }
     
 }

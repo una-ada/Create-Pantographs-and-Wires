2dd76e0c4d3879a99560e306e8cdca6ebd777d1b
Una Ada <trewbot@gmail.com>
2025-08-15 09:24:07 -0700

    Replace utility with catnip

--- common/src/main/java/de/mrjulsen/paw/blockentity/PantographBlockEntity.java
+++ common/src/main/java/de/mrjulsen/paw/blockentity/PantographBlockEntity.java
@@ -11,9 +11,9 @@ import org.joml.Vector3f;
 
 import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
 import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour;
-import com.simibubi.create.foundation.utility.AnimationTickHolder;
-import com.simibubi.create.foundation.utility.animation.LerpedFloat;
-import com.simibubi.create.foundation.utility.animation.LerpedFloat.Chaser;
+import net.createmod.catnip.animation.AnimationTickHolder;
+import net.createmod.catnip.animation.LerpedFloat;
+import net.createmod.catnip.animation.LerpedFloat.Chaser;
 
 import de.mrjulsen.paw.util.Const;
 import de.mrjulsen.wires.WireNetwork;
